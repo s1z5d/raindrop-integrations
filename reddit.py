e90070ce25e82c215d7bf29e04f039fda8964f37
Siddhant Virmani <siddhantvirmani@gmail.com>
2020-12-18 16:56:58 +1100

    Finish reddit

--- reddit.py
+++ reddit.py
@@ -79,12 +79,42 @@ reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,
 links = []
 i = 0
 with open('reddit.txt', 'r') as f:
-    seen = f.readlines()
+    seen = f.read()
 
 for item in reddit.user.me().saved(limit=None):
     if item.permalink in seen: break
 
-    print(item.subreddit.display_name, item.permalink, item.title if type(item) is praw.models.Submission else '')
-    i += 1
+    if type(item) is praw.models.Submission:
+        links.append({
+            'link': item.permalink,
+            'title': '[Reddit: ' + item.subreddit.display_name + '] ' + item.title,
+            'sub': item.subreddit.display_name,
+            'content': item.url + '\n' + item.selftext
+        })
+    else:
+        links.append({
+            'link': item.permalink + '?context=10000',
+            'title': '[Reddit Commend: ' + item.subreddit.display_name + '] ' + item.submission.title,
+            'sub': item.subreddit.display_name,
+            'content': item.body
+        })
+    i = i + 1
+    print(item.permalink)
 
-print(i)
+headers = {
+    'Authorization': 'Bearer ' + RAINDROP_TOKEN
+}
+
+with open('reddit.txt', 'a+') as f:
+    for i, link in enumerate(links):
+        f.write(link['link'] + '\n')
+        payload = {
+            'link': 'https://old.reddit.com' + link['link'],
+            'tags': ['reddit'],
+            'excerpt': 'https://old.reddit.com' + link['link'] + '\n' + link['content'],
+            'title': link['title']
+        }
+        r = requests.post('https://api.raindrop.io/rest/v1/raindrop', headers=headers, json=payload)
+        print(str(i + 1) + ' of ' + str(len(links)))
+        print(r.content)
+        time.sleep(1)
