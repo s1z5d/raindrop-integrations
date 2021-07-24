ccb7c6115801507203267283fa8f982d95a162f9
Siddhant Virmani <siddhantvirmani@gmail.com>
2021-07-24 23:02:21 +1000

    i hate twitter

--- twitter.py
+++ twitter.py
@@ -54,22 +54,107 @@ pass_elem = driver.find_element_by_css_selector("input[name*='pass']")
 pass_elem.send_keys(TWITTER_PASSWORD)
 pass_elem.send_keys(Keys.ENTER)
 
-time.sleep(5)
+time.sleep(2)
 
-if (driver.find_element_by_css_selector("input[name*='email']")):
+# sometimes, twitter will ask you to verify with your username
+if (len(driver.find_elements_by_css_selector("input[name*='email']")) != 0):
     un_elem = driver.find_element_by_css_selector("input[name*='email']")
     un_elem.send_keys(TWITTER_USERNAME)
     pass_elem = driver.find_element_by_css_selector("input[name*='pass']")
     pass_elem.send_keys(TWITTER_PASSWORD)
     pass_elem.send_keys(Keys.ENTER)
 
-time.sleep(5)
+# time.sleep(2)
 
 otp_elem = driver.find_element_by_id("challenge_response")
 totp = pyotp.TOTP(TWITTER_OTP)
 otp_elem.send_keys(totp.now())
 otp_elem.send_keys(Keys.ENTER)
 
-
 driver.get('https://twitter.com/i/bookmarks')
 
+links = []
+titles = []
+texts = []
+
+with open('twitter.txt', 'a+') as f:
+    f.seek(0)
+    existing_links = f.read()
+
+# scroll to bottom of page https://stackoverflow.com/a/43299513
+SCROLL_PAUSE_TIME = 5
+
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+already_added = False
+while True:
+    xpath_elems = driver.find_elements_by_xpath("/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/section/div/div/div/div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]/a")
+    if len(xpath_elems) != 0:
+        link = xpath_elems[-1].get_attribute("href")
+        if link in existing_links:
+            already_added = True
+    
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    i = 1
+    while True:
+        xpath_elems = driver.find_elements_by_xpath("/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/section/div/div/div[{}]/div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]/a".format(i))
+        if len(xpath_elems) == 0:
+            break
+        elif len(xpath_elems) == 1:
+            link = xpath_elems[0].get_attribute("href")
+            if link in links:
+                i = i + 1
+                continue
+            links.append(link)
+            title = driver.find_element_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/section/div/div/div[{}]/div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]/div[1]/a'.format(i))
+            title = title.text.replace("\n", '')
+            index = title.find('@')
+            title = "Tweet from " + title[:index] + ' (' + title[index:] +  ')'
+            titles.append(title)
+            text_elems = driver.find_elements_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/section/div/div/div[{}]/div/div/article/div/div/div/div[2]/div[2]/div[2]/div[1]/div/span'.format(i))
+            text = ''
+            if len(text_elems) != 0:
+                text = text_elems[0].text
+            texts.append(text)
+        i = i + 1
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height or already_added:
+        break
+    last_height = new_height
+
+
+
+driver.close()
+
+headers = {
+    'Authorization': 'Bearer ' +  RAINDROP_TOKEN,
+}
+
+links = list(filter(lambda x: x not in existing_links, links))
+l = len(links)
+print('Adding ' + str(l) + ' links!')
+
+with open('twitter.txt', 'a+') as f:
+    f.seek(0)
+    for i in range(len(links)):
+        f.write(links[i] + '\n')
+        payload = {
+            'link': links[i],
+            'tags': ['twitter'],
+            'excerpt': links[i] + '\n' + titles[i] + '\n' + texts[i],
+            'title': title[i]
+        }
+        r = requests.post('https://api.raindrop.io/rest/v1/raindrop', headers=headers, json=payload)
+        print(str(i + 1) + ' out of ' + str(l) + ' links added.')
+        print(r.content)
+        time.sleep(1)
+
+print(str(l) + ' links added!')
