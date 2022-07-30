1c0b04c0fd2056316ef8ff28b2cb02946a6c6a57
Siddhant Virmani <siddhantvirmani@gmail.com>
2022-07-31 03:16:34 +1000

    use fb plugin to get past encoded links

--- facebook.py
+++ facebook.py
@@ -2,18 +2,12 @@
 
 import time
 import os
-import json
 import urllib
 import re
 
 import requests
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 import pyotp
 
@@ -27,9 +21,8 @@ FB_OTP = os.getenv('FB_OTP')
 RAINDROP_TOKEN = os.getenv('RAINDROP_TOKEN')
 
 # TODO:
-# - better way to handle already added links
-#   - try to only get first 10 links or so, and go back and check?
-#   - while scrolling, get the bottom most link after a scroll and check if it already exists 
+# - potentially use xpath counting to fix perf issue when looking for links while scrolling
+#   - keep count of posts we've gone through and limit xpath search using them?
 # - find way to fix total link count
 # - error handling and reporting
 
@@ -72,7 +65,77 @@ SCROLL_PAUSE_TIME = 3
 # Get scroll height
 last_height = driver.execute_script("return document.body.scrollHeight")
 
+existing_links = []
+with open('facebook.txt', 'a+') as f:
+    f.seek(0)
+    existing_links = f.read()
+
+payloads = []
+links = []
+found = False
 while True:
+    # Grab links from titles (which always exist)
+    # Unfortunately, we will grab _all_ the titles in a page after every scroll.
+    # While this is mostly acceptable for incremental runs, it will be very slow for the first run.
+    title_elems = driver.find_elements_by_xpath("/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div[2]/div/div/div/div/div[2]/div/div/div/div/div[2]/a")
+    _links = list(map(lambda e: e.get_attribute("href"), title_elems))
+    # Grab "new" links -- these are the ones we might want to test
+    new_links_to_test = list(filter(lambda l: l not in links, _links))
+    # Thanks to Zuck, every link is now encoded with tracking data and not unique. Using an old plugin link, we can get a unique URL and check against existing links.
+    # https://news.ycombinator.com/item?id=32118095
+    for link in new_links_to_test:
+        # "pfbid" denotes an encoded link
+        # if the link is not encoded, we don't need to pass it through the plugin
+        if "pfbid" in link:
+            # Open new tab
+            driver.execute_script("window.open()")
+            tabs = driver.window_handles
+            driver.switch_to.window(tabs[1])
+            driver.get("https://www.facebook.com/plugins/post.php?href=" + link)
+            # time.sleep(3) # Wait for page to load
+            unique_link = driver.find_element_by_xpath("//a[contains(@href,'/posts/')]").get_attribute("href")
+            # Close tab
+            driver.execute_script("window.close()")
+            driver.switch_to.window(tabs[0])
+        else:
+            unique_link = link
+
+        cleaned_link = re.sub(r"[\?|&](fbclid|h)=.*", '', urllib.parse.unquote(unique_link.replace("https://l.facebook.com/l.php?u=", '')))
+        if cleaned_link in existing_links: 
+            found = True
+            break
+        
+        # Try and grab "title"
+        index = next(i for i, e in enumerate(title_elems) if e.get_attribute("href") == link) # Idiomatic...
+        subtitle_elems = driver.find_elements_by_xpath("//a[contains(@href, '{}')]//..//span[contains(text(), 'Saved from')]//child::a[1]".format(link))
+        if (len(subtitle_elems) == 0):
+            # One of those weird saved posts that only contains a link, and no reference to a post...
+            # We construct the payload accordingly
+            title = title_elems[index].text # Use the main title as title
+            tags = ['facebook'] if 'facebook.com' in cleaned_link else ['facebook_link']
+            payload = {
+                'link': cleaned_link,
+                'tags': tags,
+                'excerpt': title + '\n' + cleaned_link,
+                'title': title
+            }
+            payloads.append(payload)
+        else:
+            title = subtitle_elems[0].text
+            alt_title = title_elems[index].text
+            payload = {
+                'link': cleaned_link,
+                'tags': ['facebook'],
+                'excerpt': alt_title + '\n' + cleaned_link,
+                'title': title + ' from Facebook'
+            }
+            payloads.append(payload)
+    
+    if found: break
+    
+    # Add the new links to the main list, since they have been checked
+    links.extend(new_links_to_test)
+
     # Scroll down to bottom
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
@@ -85,86 +148,23 @@ while True:
         break
     last_height = new_height
 
-total_links = 0
-links = []
-titles = []
-payloads = []
-
-# This bit works for links that are saved from posts
-link_elems = driver.find_elements_by_xpath("//span[contains(text(), 'Saved from')]//child::a[1]")
-title_elems = driver.find_elements_by_xpath("//span[contains(text(), 'Saved from')]//child::a[1]//child::span[1]")
-
-for le, te in zip(link_elems, title_elems):
-    # clean link
-    link = le.get_attribute("href")
-    cleaned_link = re.sub(r"[\?|&](fbclid|h)=.*", '', urllib.parse.unquote(link.replace("https://l.facebook.com/l.php?u=", '')))
-    links.append(cleaned_link)
-    titles.append(te.get_attribute("innerHTML"))
-
-with open('facebook.txt', 'a+') as f:
-    f.seek(0)
-    existing_links = f.read()
-    for i in range(len(links)):    
-        if links[i] not in existing_links:
-            f.write(links[i] + '\n')
-            payload = {
-                'link': links[i],
-                'tags': ['facebook'],
-                'excerpt': links[i],
-                'title': titles[i] + ' from Facebook'
-            }
-            payloads.append(payload)
-
-total_links += len(links)
-links = []
-titles = []
-
-# This bit works for links that don't have any posts associated with them (only seen in old saved links).
-i = 1
-while True:
-    xpath_elems = driver.find_elements_by_xpath('/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div[2]/div[{}]/div/div/div/div[2]/a | /html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div[2]/div[{}]/div/div/div/div[2]/div/div[2]/div/div[2]/span'.format(i, i))
-    if len(xpath_elems) == 0:
-        break
-    elif len(xpath_elems) == 1:
-        # clean link
-        link = xpath_elems[0].get_attribute("href")
-        cleaned_link = re.sub(r"[\?|&](fbclid|h)=.*", '', urllib.parse.unquote(link.replace("https://l.facebook.com/l.php?u=", '')))
-        links.append(cleaned_link)
-        title = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div[2]/div[{}]/div/div/div/div[2]/a/span/span'.format(i)).get_attribute("innerHTML")
-        titles.append(title)
-    i = i + 1
-
 driver.close()
 
-total_links += len(links)
-
-with open('facebook.txt', 'a+') as f:
-    f.seek(0)
-    existing_links = f.read()
-    for i in range(len(links)):    
-        if links[i] not in existing_links:
-            f.write(links[i] + '\n')
-            payload = {
-                'link': links[i],
-                'tags': ['facebook_link'],
-                'excerpt': links[i] + '\n' + titles[i],
-            }
-            payloads.append(payload)
-
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
-
-if file_len('facebook.txt') != total_links:
-    print("ERROR!!!!! Number of links didn't match, please check manually.")
+n = len(payloads)
+print('Adding {} links!'.format(n))
 
 # add links to raindrop
 headers = {
     'Authorization': 'Bearer ' +  RAINDROP_TOKEN,
 }
-for p in payloads:    
-    r = requests.post('https://api.raindrop.io/rest/v1/raindrop', headers=headers, json=p)
-    print(r.content)
-    time.sleep(1)
+
+with open('facebook.txt', 'a+') as f:
+    f.seek(0)
+    for i in range(len(payloads)):
+        p = payloads[i]
+        print(p['link'] + ' ' + str(i + 1) + '/' + str(n))
+        f.write(p['link'] + '\n')
+        r = requests.post('https://api.raindrop.io/rest/v1/raindrop', headers=headers, json=p)
+        print(r.content)
+        print()
+        time.sleep(1)
