6deb87d74963d4446dfd292f7475307561c3cf3b
Siddhant Virmani <siddhantvirmani@gmail.com>
2024-05-18 23:13:33 +1000

    update fb/x

--- twitter.py
+++ twitter.py
@@ -91,11 +91,11 @@ with open('twitter.txt', 'a+') as f:
     f.seek(0)
     existing_links = f.read()
 
-link_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div/div/div[{}]/div/div/article/div/div/div[2]/div[2]/div[1]/div/div[1]/div/div/div[2]/div/div[3]/a"
-title_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div/div/div[{}]/div/div/article/div/div/div[2]/div[2]/div[1]/div/div[1]/div/div"
-text_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div/div/div[{}]/div/div/article/div/div/div[2]/div[2]/div[2]"
-reply_text_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div/div/div[{}]/div/div/article/div/div/div[2]/div[2]/div[3]"
-alt_text_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div/div/div[{}]/div/div/article/div/div/div[2]/div[2]/div[3]/div/div/div/div/div/div/a/div/div[2]/div/img"
+link_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[{}]/div/div/article/div/div/div[2]/div[2]/div[1]/div/div[1]/div/div/div[2]/div/div[3]/a"
+title_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[{}]/div/div/article/div/div/div[2]/div[2]/div[1]/div/div[1]/div/div"
+text_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[{}]/div/div/article/div/div/div[2]/div[2]/div[2]"
+reply_text_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[{}]/div/div/article/div/div/div[2]/div[2]/div[3]"
+alt_text_xpath = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/section/div/div/div[{}]/div/div/article/div/div/div[2]/div[2]/div[3]/div[1]/div/div/div/div/div/a/div/div[2]/div/img"
 
 
 
